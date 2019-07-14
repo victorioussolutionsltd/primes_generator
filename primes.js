b77a0776adba9cdc43b54abc042a908fcb3fddfe
Peter Malecki <peter.malecki@hotmail.co.uk>
2019-07-14 23:48:17 +0100

    Made test pass

--- primes.js
+++ primes.js
@@ -1,4 +1,15 @@
 export const isPrime = (number) => {
 
-    return false;
+    if (number===2){
+        return true;
+    }
+
+
+    for (let i = 3; i <= number-1; i+=2){
+        if(number%2 === 0){
+            return false;
+        }
+    }
+
+    return true;
 }
