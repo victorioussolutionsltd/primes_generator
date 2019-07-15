223b7cb675e15155ab7571f776a03f371a853c4b
Peter Malecki <peter.malecki@hotmail.co.uk>
2019-07-15 23:20:59 +0100

    Added simple validation for missing argument when program executed

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import { getStringPrimeMultiplicationTableFromArrays,  multiplicationTable } from "./primes";
 
+if ( process.argv.length === 2){
+    console.log("Numeric input (N) for multiplication table is missing!");
+    process.exit();
+}
 
 console.log(getStringPrimeMultiplicationTableFromArrays(multiplicationTable(process.argv[2])));
